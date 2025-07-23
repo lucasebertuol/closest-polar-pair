5bbcb4dda9292498caad6b8e9cf308ca883b4f4c
lucasebertuol <lucasebertuol@gmail.com>
2025-07-23 15:27:25 -0300

    Detail

--- main.m
+++ main.m
@@ -9,7 +9,7 @@ results = [];
 results_deg = [];
 tic
 for n = N
-    disp("Calculando para n = " + num2str(n));
+    disp("Calculando para n = " + num2str(n) + "...");
     center = round(n / 2);
     % [rho, theta, theta_deg, x, y;]
     polar_points = [];
