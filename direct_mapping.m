61332d85a11baa0bdee0ea01b707626361f149a8
lucasebertuol <lucasebertuol@gmail.com>
2025-07-24 21:24:11 -0300

    DM Adjust

--- direct_mapping.m
+++ direct_mapping.m
@@ -5,21 +5,25 @@ clear; clc; close all;
 
 % ---------------------------------- Settings ---------------------------------
 N = 201; % Image Size
-Deltarho = 0.0049999;
+Deltarho = 0.005;
 Deltatheta = 0.57294; % In degrees
+dorig = sqrt((0.0049999)^2 + (0.57294)^2)
 % -----------------------------------------------------------------------------
 
 center = round(N/2);
 rho = sqrt(center^2 + center^2);
-Rho = ceil(rho / Deltarho);
+Rho = ceil(rho / Deltarho + 1);
 Theta = ceil(180/Deltatheta + 1);
 
+Deltarho = rho / (Rho - 1);
+Deltatheta = 180 / (Theta - 1);
+dactual = sqrt(Deltarho^2 + Deltatheta^2)
 
 % inim = double(rgb2gray(imread('eagle_512x512.png')));
 % inim = inim(1:511, 1:511);
 inim = ones(N, N);
-figure
-imshow(inim);
+% figure
+% imshow(inim);
 
 outim = zeros(Rho, Theta);
 
@@ -37,7 +41,7 @@ for r = 1:center
         end
     end
 end
-figure
-imshow(mat2gray(outim))
+% figure
+% imshow(mat2gray(outim))
 
 preservation_percentage = sum(outim(:) == 1) / ((N-1)/2 * N + N) * 100
