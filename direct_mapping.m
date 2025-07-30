d0b6705d9a1350abb1345f6459f0dab07b2f7247
lucasebertuol <lucasebertuol@gmail.com>
2025-07-29 22:00:10 -0300

    Improvements

--- direct_mapping.m
+++ direct_mapping.m
@@ -4,19 +4,19 @@
 clear; clc; close all;
 
 % ---------------------------------- Settings ---------------------------------
-N = 201; % Image Size
-Deltarho = 0.0049999;
-Deltatheta = 0.57294; % In degrees
-dorig = sqrt((0.0049999)^2 + (0.57294)^2)
+N = 101; % Image Size
+dorig = sqrt((0.70354)^2 + (0.57873)^2)
+Deltarho = 1.15*(dorig/sqrt(2));
+Deltatheta = Deltarho; % In degrees
 % -----------------------------------------------------------------------------
 
 center = round(N/2);
 rho = sqrt((center-1)^2 + (center-1)^2);
 Rho = ceil(rho / Deltarho + 1);
-Theta = ceil(180/Deltatheta + 1);
+Theta = ceil(360/Deltatheta + 1);
 
-Deltarho = rho / (Rho - 1);
-Deltatheta = 180 / (Theta - 1);
+% Deltarho = rho / (Rho - 1)
+% Deltatheta = 360 / (Theta - 1)
 dactual = sqrt(Deltarho^2 + Deltatheta^2)
 
 % inim = double(rgb2gray(imread('eagle_512x512.png')));
@@ -26,22 +26,47 @@ inim = ones(N, N);
 % imshow(inim);
 
 outim = zeros(Rho, Theta);
+count = 0;
 
 % Direct mapping
-for r = 1:center
+for r = 1:N
     for c = 1:N
         x = c - center;
-        y = center - r;
+        y =  center-r;
 
         rho = round(sqrt(x^2+y^2) / Deltarho) + 1;
-        theta = round(atan2d(y, x) / Deltatheta) + 1;
+        theta = round((atan2d(y, x)+180) / Deltatheta) + 1;
 
         if rho <= Rho && theta <= Theta
+            if outim(rho, theta) ~= 0
+                count = count+1;
+            end
+            if (rho == 86 && theta ==313)
+                [r, c]
+                [sqrt(x^2+y^2) / Deltarho + 1, (atan2d(y, x)+180) / Deltatheta + 1]
+            end
             outim(rho, theta) = inim(r, c);
         end
     end
 end
+if count > 0
+  disp('Não salvou todos os pontos...')
+  count
+end
 % figure
 % imshow(mat2gray(outim))
 
-preservation_percentage = sum(outim(:) == 1) / ((N-1)/2 * N + N) * 100
+preservation_percentage = sum(outim(:)) / (N^2) * 100
+
+%d para os pontos próximos a 45 graus
+rho1 = (sqrt((N - center)^2+(N - center)^2) / Deltarho)+1;
+rho2 = (sqrt((N-1 - center)^2+(N - center)^2) / Deltarho)+1;
+theta1 = ((atan2d(center - N, N - center)+ 180)/ Deltatheta)+1;
+theta2 = ((atan2d(center - N - 1, N - center)+ 180)/ Deltatheta)+1;
+d = sqrt((rho1 - rho2)^2 + (theta1 - theta2)^2)
+
+rho1 = (sqrt((2 - center)^2+(90 - center)^2) / Deltarho) + 1;
+rho2 = (sqrt((2 - center)^2+(91 - center)^2) / Deltarho) + 1;
+theta1 = ((atan2d(center-2, 90 - center) + 180)/ Deltatheta) + 1;
+theta2 = ((atan2d(center - 2, 91 - center) + 180)/ Deltatheta) + 1;
+d = sqrt((rho1 - rho2)^2 + (theta1 - theta2)^2)
